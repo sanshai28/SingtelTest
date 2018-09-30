6cb8154f72c24c4a3d03c0fd66faf9536eefe72f
Sanchayan Paramsothy <sanchayan.para@ncs.com.sg>
2018-10-01 05:20:10 +0800

    BONUS Implemented sing method based on langugae

--- src/main/java/assignment/FlyableAnimal.java
+++ src/main/java/assignment/FlyableAnimal.java
@@ -1,4 +1,4 @@
-package main.java;
+package assignment;
 
 public interface FlyableAnimal  {
 
--- src/main/java/assignment/Rooster.java
+++ src/main/java/assignment/Rooster.java
@@ -1,12 +1,45 @@
 package assignment;
 
+import java.util.HashMap;
+
 public class Rooster extends Chicken {
 	
+	private String language;
+	private String sound;
 	
-	public String sing() {
-		String sing = "Cock-a-doodle-doo";
-		System.out.println(sing);
-		return sing;
+	
+	 
+	 
+	 
+	 
+	public String sing(String language) {
+		 HashMap<String,String> roosterSound = new HashMap<String,String>();
+		 String sound = null;
+		 roosterSound.put("Danish", "kykyliky");
+			roosterSound.put("Dutch", "kukeleku");
+			roosterSound.put("Finnish", "kukko kiekuu");
+			roosterSound.put("French", "cocorico");
+			roosterSound.put("German", "kikeriki");
+			roosterSound.put("Greek", "kikiriki");
+			roosterSound.put("Hebrew", "coo-koo-ri-koo");
+			roosterSound.put("Hungarian", "kukuriku");
+			roosterSound.put("Italian", "chicchirichi");
+			roosterSound.put("Japanese", "ko-ke-kok-ko-o");
+			roosterSound.put("Portuguese", "cucurucu");
+			roosterSound.put("Russian", "kukareku");
+			roosterSound.put("Swedish", "kuckeliku");
+			roosterSound.put("Turkish", "kuk-kurri-kuuu");
+			roosterSound.put("Urdu", "kuklooku");
+			sound = roosterSound.get(language);
+			if(sound == null) {
+				return  "Cock-a-doodle-doo";
+			}
+			
+			return sound;
+			
+			
+			
+		
 	}
 
 }
