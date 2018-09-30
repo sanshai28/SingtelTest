1ba3eb022f8761a55cf831e7948a8eb9d0a493fc
Sanchayan Paramsothy <sanchayan.para@ncs.com.sg>
2018-10-01 04:26:32 +0800

    A3 Implemented Rooster model

---
+++ src/main/java/assignment/Rooster.java
@@ -0,0 +1,12 @@
+package assignment;
+
+public class Rooster extends Chicken {
+	
+	
+	public String sing() {
+		String sing = "Cock-a-doodle-doo";
+		System.out.println(sing);
+		return sing;
+	}
+
+}
