af6307bcffdfe55d6422ca9048fe6c21fbe7517a
Sanchayan Paramsothy <sanchayan.para@ncs.com.sg>
2018-10-01 04:30:04 +0800

    B1 Impemented Fish Model

---
+++ src/main/java/assignment/Fish.java
@@ -0,0 +1,19 @@
+package assignment;
+
+public class Fish implements SwimmableAnimal {
+	
+	
+	
+	
+	public  Fish() {
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	public void swim() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
