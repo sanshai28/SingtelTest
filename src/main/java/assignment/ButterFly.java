90d46166ad7b29ef8e58388829fab415188ccc4b
Sanchayan Paramsothy <sanchayan.para@ncs.com.sg>
2018-10-01 04:39:50 +0800

    D1 Implemented Butterfly model

---
+++ src/main/java/assignment/ButterFly.java
@@ -0,0 +1,10 @@
+package assignment;
+
+public class ButterFly implements Insect {
+
+	public boolean fly() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
---
+++ src/main/java/assignment/Insect.java
@@ -0,0 +1,6 @@
+package assignment;
+
+public interface Insect {
+	boolean fly();
+    
+}
